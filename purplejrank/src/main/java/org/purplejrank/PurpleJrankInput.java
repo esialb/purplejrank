bce14c441c431b00fb8404ec991ebc2021eee6f3
robin <robin@robinkirkman.com>
2014-03-08 00:20:27 -0500

    Stub extension of ObjectInputStream

--- purplejrank/src/main/java/org/purplejrank/PurpleJrankInput.java
+++ purplejrank/src/main/java/org/purplejrank/PurpleJrankInput.java
@@ -1,5 +1,174 @@
 package org.purplejrank;
 
-public class PurpleJrankInput {
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.NotActiveException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectInputValidation;
+
+public class PurpleJrankInput extends ObjectInputStream implements ObjectInput {
+
+	public PurpleJrankInput() throws IOException, SecurityException {
+		// TODO Auto-generated constructor stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void readFully(byte[] b) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readFully(byte[] b, int off, int len) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int skipBytes(int n) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean readBoolean() throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public byte readByte() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int readUnsignedByte() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public short readShort() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int readUnsignedShort() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public char readChar() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int readInt() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long readLong() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float readFloat() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double readDouble() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String readLine() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String readUTF() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object readObjectOverride() throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int read(byte[] b) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int read(byte[] b, int off, int len) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long skip(long n) throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int available() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object readUnshared() throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		return super.readUnshared();
+	}
+
+	@Override
+	public void defaultReadObject() throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		super.defaultReadObject();
+	}
+
+	@Override
+	public GetField readFields() throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		return super.readFields();
+	}
+
+	@Override
+	public void registerValidation(ObjectInputValidation obj, int prio)
+			throws NotActiveException, InvalidObjectException {
+		// TODO Auto-generated method stub
+		super.registerValidation(obj, prio);
+	}
 
 }
