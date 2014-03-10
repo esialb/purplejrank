d267577e184872720c921f547a647732b913d477
robin <robin@robinkirkman.com>
2014-03-10 01:32:28 -0400

    Test showing objects not readable by JDK streams but readable by Jrank

---
+++ purplejrank/src/test/java/org/purplejrank/TestJDKUnparseableStream.java
@@ -0,0 +1,53 @@
+package org.purplejrank;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.StreamCorruptedException;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestJDKUnparseableStream {
+	public static class A implements Serializable {
+		public Object fail = Arrays.asList("this", "will", "fail");
+		
+		private void writeObject(ObjectOutputStream out) throws IOException {
+			
+		}
+	}
+	
+	@Test
+	public void testJdkFails() throws Exception {
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		ObjectOutputStream out = new ObjectOutputStream(bout);
+		out.writeObject(new A());
+		out.close();
+		
+		ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bout.toByteArray()));
+		try {
+			in.readObject();
+			Assert.fail();
+		} catch(StreamCorruptedException e) {
+			// expected
+		} finally {
+			in.close();
+		}
+	}
+	
+	@Test
+	public void testJrankSucceeds() throws Exception {
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		ObjectOutputStream out = new PurpleJrankOutput(bout);
+		out.writeObject(new A());
+		out.close();
+		
+		ObjectInputStream in = new PurpleJrankInput(new ByteArrayInputStream(bout.toByteArray()));
+		in.readObject();
+		in.close();
+	}
+}
