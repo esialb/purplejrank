bddf8c54bc1d15919c3792962dfa87f7b716937e
robin <robin@robinkirkman.com>
2014-03-09 21:45:20 -0400

    Forgot to check in the collections classes

---
+++ purplejrank/src/main/java/org/purplejrank/CollectionsJrankInput.java
@@ -0,0 +1,82 @@
+package org.purplejrank;
+
+import java.io.IOException;
+import java.io.NotActiveException;
+import java.lang.reflect.Field;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CollectionsJrankInput extends PurpleJrankInput {
+
+	public CollectionsJrankInput(ReadableByteChannel in) throws IOException {
+		super(in);
+	}
+
+	public CollectionsJrankInput(ReadableByteChannel in, ClassLoader cl)
+			throws IOException {
+		super(in, cl);
+	}
+
+	@Override
+	protected Object newArray(JrankClass desc, int size) {
+		return Arrays.asList(new Object[size]);
+	}
+	
+	@Override
+	protected Object newOrdinaryObject(JrankClass desc) {
+		return new TreeMap<String, Object>();
+	}
+	
+	@Override
+	protected Class<?> resolveClass(String name) throws IOException, ClassNotFoundException {
+		try {
+			return super.resolveClass(name);
+		} catch(ClassNotFoundException e) {
+			return Map.class;
+		}
+	}
+	
+	@Override
+	protected void skipOptionalData() throws IOException, ClassNotFoundException {
+		setBlockMode(false);
+		for(byte b = peek(); b != JrankConstants.WALL; b = peek()) {
+			switch(b) {
+			case JrankConstants.BLOCK_DATA:
+				setBlockMode(true).setBlockMode(false);
+				break;
+			case JrankConstants.FIELDS:
+				defaultReadObject();
+				break;
+			default:
+				readObject0(true);
+			}
+			setBlockMode(false);
+		}
+	}
+	
+	@Override
+	public void defaultReadObject() throws IOException, ClassNotFoundException {
+		JrankContext ctx = context.peekLast();
+		if(ctx == JrankContext.NO_CONTEXT)
+			throw new NotActiveException();
+		JrankGetFields fields = readFields();
+		for(String name : ctx.getType().getFieldNames()) {
+			((Map<String, Object>) ctx.getObject()).put(name, fields.get(name, null));
+		}
+	}
+	
+	@Override
+	protected void checkSerialVersion(JrankClass desc, Class<?> cls) throws ClassNotFoundException {
+	}
+	
+	@Override
+	protected void setArrayElement(Object array, int index, Object value) {
+		((List<Object>) array).set(index, value);
+	}
+
+}
---
+++ purplejrank/src/test/java/org/purplejrank/CollectionsJrankInputTest.java
@@ -0,0 +1,65 @@
+package org.purplejrank;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+
+public class CollectionsJrankInputTest {
+	public static class Missing implements Serializable {
+		private static final long serialVersionUID = 0;
+		
+		private int i = 1;
+		
+		private void writeObject(ObjectOutputStream out) throws IOException {
+			out.defaultWriteObject();
+			out.writeInt(2);
+		}
+		
+	}
+	
+	private static class MissingMissingClassLoader extends ClassLoader {
+		public MissingMissingClassLoader() {
+			super(MissingMissingClassLoader.class.getClassLoader());
+		}
+		
+		@Override
+		public Class<?> loadClass(String name) throws ClassNotFoundException {
+			Class<?> cls = super.loadClass(name);
+			if(cls == Missing.class)
+				throw new ClassNotFoundException();
+			return cls;
+		}
+	}
+	
+	@Test
+	public void testMissingAsNull() throws Exception {
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		try {
+			StreamWritableByteChannel ch = new StreamWritableByteChannel(bout);
+			ObjectOutputStream out = new PurpleJrankOutput(ch);
+			out.writeObject(new Missing());
+			out.writeObject(new Missing[4]);
+			out.close();
+		} catch(Exception e) {
+			Assume.assumeNoException(e);;
+		}
+		
+		byte[] buf = bout.toByteArray();
+
+		ByteArrayInputStream bin = new ByteArrayInputStream(buf);
+		StreamReadableByteChannel ch = new StreamReadableByteChannel(bin);
+		ObjectInputStream in = new CollectionsJrankInput(ch, new MissingMissingClassLoader());
+
+		Assert.assertEquals("{i=1}", in.readObject().toString());
+		Assert.assertEquals("[null, null, null, null]", in.readObject().toString());
+		
+		in.close();
+	}
+}
