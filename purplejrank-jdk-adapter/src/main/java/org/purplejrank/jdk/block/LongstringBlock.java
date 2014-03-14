4b3cd5968d90682205f7d8b44912645efa79aa8b
robin <robin@robinkirkman.com>
2014-03-14 17:27:05 -0400

    StringRule

--- purplejrank-jdk-adapter/src/main/java/org/purplejrank/jdk/block/LongstringBlock.java
+++ purplejrank-jdk-adapter/src/main/java/org/purplejrank/jdk/block/LongstringBlock.java
@@ -6,8 +6,9 @@ import org.purplejrank.jdk.Block;
 import org.purplejrank.jdk.JdkBlock;
 import org.purplejrank.jdk.JdkStream;
 import org.purplejrank.jdk.rule.ObjectRule;
+import org.purplejrank.jdk.rule.StringRule;
 
-public class LongstringBlock extends JdkBlock implements ObjectRule {
+public class LongstringBlock extends JdkBlock implements ObjectRule, StringRule {
 
 	public LongstringBlock(JdkStream jdk) {
 		super(jdk);
