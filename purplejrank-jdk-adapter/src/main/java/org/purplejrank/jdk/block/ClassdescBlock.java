db62c249ea997ee038dc2a5a67473f7c0fdc1996
robin <robin@robinkirkman.com>
2014-03-14 22:04:11 -0400

    Classdesc and Proxyclassdesc

--- purplejrank-jdk-adapter/src/main/java/org/purplejrank/jdk/block/ClassdescBlock.java
+++ purplejrank-jdk-adapter/src/main/java/org/purplejrank/jdk/block/ClassdescBlock.java
@@ -2,24 +2,81 @@ package org.purplejrank.jdk.block;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StreamCorruptedException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.purplejrank.jdk.Block;
 import org.purplejrank.jdk.JdkBlock;
 import org.purplejrank.jdk.JdkStream;
 import org.purplejrank.jdk.WiredBlock;
+import org.purplejrank.jdk.rule.ContentRule;
 import org.purplejrank.jdk.rule.NewclassdescRule;
 import org.purplejrank.jdk.rule.ObjectRule;
+import org.purplejrank.jdk.rule.StringRule;
+import org.purplejrank.jdk.rule.SuperclassdescRule;
 
 public class ClassdescBlock extends JdkBlock implements ObjectRule, NewclassdescRule, WiredBlock {
 
+	protected String className;
+	protected long serialVersionUID;
+	protected byte classDescFlags;
+	protected List<Field> fields = new ArrayList<Field>();
+	protected List<ContentRule> classAnnotation = new ArrayList<ContentRule>();
+	protected SuperclassdescRule superClassDesc;
+	
+	public class Field extends JdkBlock {
+		protected byte typeCode;
+		protected String fieldName;
+		protected StringRule className;
+		
+		public Field(JdkStream jdk) {
+			super(jdk);
+		}
+
+		@Override
+		public Field parse() throws IOException {
+			typeCode = jdk.readByte();
+			fieldName = jdk.readUTF();
+			switch(typeCode) {
+			case 'L': case '[':
+				className = jdk.readBlock(StringRule.class);
+				break;
+			case 'B': case 'C': case 'D': case 'F': case 'I': case 'J': case 'S': case 'Z':
+				break;
+			default:
+				throw new StreamCorruptedException("Invalid typecode:" + typeCode);
+			}
+			return this;
+		}
+
+		@Override
+		public void writeJrank(OutputStream out) throws IOException {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 	public ClassdescBlock(JdkStream jdk) {
 		super(jdk);
 	}
 
 	@Override
 	public Block parse() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		className = jdk.readUTF();
+		serialVersionUID = jdk.readLong();
+		jdk.wireBlock(this);
+		classDescFlags = jdk.readByte();
+		short fieldCount = jdk.readShort();
+		for(int i = 0; i < fieldCount; i++)
+			fields.add(new Field(jdk).parse());
+		Block a;
+		while((a = jdk.readBlock()) instanceof ContentRule)
+			classAnnotation.add((ContentRule) a);
+		EndblockdataBlock.class.cast(a);
+		superClassDesc = jdk.readBlock(SuperclassdescRule.class);
+		return this;
 	}
 
 	@Override
--- purplejrank-jdk-adapter/src/main/java/org/purplejrank/jdk/block/ProxyclassdescBlock.java
+++ purplejrank-jdk-adapter/src/main/java/org/purplejrank/jdk/block/ProxyclassdescBlock.java
@@ -2,24 +2,40 @@ package org.purplejrank.jdk.block;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.purplejrank.jdk.Block;
 import org.purplejrank.jdk.JdkBlock;
 import org.purplejrank.jdk.JdkStream;
 import org.purplejrank.jdk.WiredBlock;
+import org.purplejrank.jdk.rule.ContentRule;
 import org.purplejrank.jdk.rule.NewclassdescRule;
 import org.purplejrank.jdk.rule.ObjectRule;
+import org.purplejrank.jdk.rule.SuperclassdescRule;
 
 public class ProxyclassdescBlock extends JdkBlock implements ObjectRule, NewclassdescRule, WiredBlock {
 
+	protected List<String> proxyInterfaceNames = new ArrayList<String>();
+	protected List<ContentRule> classAnnotation = new ArrayList<ContentRule>();
+	protected SuperclassdescRule superClassDesc;
+	
+	
 	public ProxyclassdescBlock(JdkStream jdk) {
 		super(jdk);
 	}
 
 	@Override
 	public Block parse() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		int count = jdk.readInt();
+		for(int i = 0; i < count; i++)
+			proxyInterfaceNames.add(jdk.readUTF());
+		Block a;
+		while((a = jdk.readBlock()) instanceof ContentRule)
+			classAnnotation.add((ContentRule) a);
+		EndblockdataBlock.class.cast(a);
+		superClassDesc = jdk.readBlock(SuperclassdescRule.class);
+		return this;
 	}
 
 	@Override
